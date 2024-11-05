9cb650d54d2d34e110e2d6a7446e7b334689ef7d
wzr <378544604@qq.com>
2024-11-05 21:02:12 +0800

    commit from vscode

---
+++ demo1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    double lirun = 0;
+    double ticheng = 0;
+    int i = 0;
+    scanf("%lf",&lirun);
+    i = (int)(lirun+10)/10
+    switch(lirun)
+    {
+        case 10:
+            ticheng = 0.1*lirun;
+            break;
+        
+    }
+
+    return 0;
+}
