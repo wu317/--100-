0e7a3bd67338d5c5d0e27bbdd1429e3f49f420bd
wzr <317410Wu@gmail.com>
2024-11-07 20:00:46 +0800

    commit from vscode

--- demo1.c
+++ demo1.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 int main()
 {
-    double lirun = 0;
+    double lirun = 0;     
     double ticheng = 0;
     int i = 0;
     long int j = 0;
     printf("请输入利润:\n");
     scanf("%lf",&lirun);
+  
+   //以下部分为向上取整
     i = (int)(lirun/100000);
-    j = (long int)lirun;
+    j = (long int)lirun;          //取余操作的两端需为整数，又因为利润为双精度浮点型，所以需要进行类型转化
     if((j % 100000) != 0)
         i++;
+  
     switch(i)
     {
         case 1:
@@ -20,18 +23,17 @@ int main()
             ticheng = 0.1*100000+0.075*(lirun-100000);
             break;
         case 3:
-            ticheng = 0.1*100000+0.075*100000+0.05*(lirun-200000);
-            break;
         case 4:
             ticheng = 0.1*100000+0.075*100000+0.05*(lirun-200000);
             break;
         case 5:
-            ticheng = 0.1*100000+0.075*100000+0.05*200000+0.03*(lirun - 400000);
-            break;
         case 6:  
             ticheng = 0.1*100000+0.075*100000+0.05*200000+0.03*(lirun - 400000);
             break;
         case 7:  
+        case 8:
+        case 9:
+        case 10:
             ticheng = 0.1*100000+0.075*100000+0.05*200000+0.03*200000-0.01*(lirun - 600000);
             break;   
         default:
